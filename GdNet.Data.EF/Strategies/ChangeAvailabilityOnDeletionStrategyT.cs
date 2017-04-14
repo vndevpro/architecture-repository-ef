79b93d8f7637b8541a20400bb751915909b3fcf7
juanonsoftware <juanonsoftware@github.com>
2017-04-14 11:04:46 +0700

    Adding EntityFramework.Extended

--- GdNet.Data.EF/Strategies/ChangeAvailabilityOnDeletionStrategyT.cs
+++ GdNet.Data.EF/Strategies/ChangeAvailabilityOnDeletionStrategyT.cs
@@ -2,11 +2,14 @@ using GdNet.Domain.Entity;
 
 namespace GdNet.Data.EF.Strategies
 {
+    /// <summary>
+    /// Just change the IsAvailable of entity to False
+    /// </summary>
     public class ChangeAvailabilityOnDeletionStrategyT<T, TId> : IDeletionStrategyT<T, TId>
         where T : class, IEditableEntityT<TId>
         where TId : new()
     {
-        public virtual void Execute(T entity)
+        public void Execute(T entity)
         {
             entity.IsAvailable = false;
         }
