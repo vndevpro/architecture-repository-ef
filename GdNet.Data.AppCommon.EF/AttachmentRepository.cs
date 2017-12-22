fc48bc974ee8e9079cb88785f39bbb5c038db520
juanonsoftware <juanonsoftware@github.com>
2017-12-22 10:38:55 +0700

    Update EntityFramework

--- GdNet.Data.AppCommon.EF/AttachmentRepository.cs
+++ GdNet.Data.AppCommon.EF/AttachmentRepository.cs
@@ -1,6 +1,7 @@
 ﻿using GdNet.Data.EF;
 using GdNet.Data.EF.Strategies;
 using GdNet.Domain.AppCommon;
+using System;
 using System.Data.Entity;
 
 namespace GdNet.Data.AppCommon.EF
@@ -12,8 +13,9 @@ namespace GdNet.Data.AppCommon.EF
         {
         }
 
-        public AttachmentRepository(IDbSet<Attachment> entities, ISavingStrategy savingStrategy, IDeletionStrategy<Attachment> deletionStrategy)
-            : base(entities, savingStrategy, deletionStrategy)
+        public AttachmentRepository(IDbSet<Attachment> entities, ISavingStrategy savingStrategy,
+            IDeletionStrategy<Attachment> deletionStrategy, IFilterStrategy<Attachment, Guid> filterStrategy)
+            : base(entities, savingStrategy, deletionStrategy, filterStrategy)
         {
         }
     }
